ae4256f04a84909f45250a7c7efbbd7f66d37ca3
Denis Fokin <Denis.Fokin@gmail.com>
2022-10-18 15:41:27 +0300

    ParameterizedType

--- framework/src/main/kotlin/org/utbot/typing/Symbol.kt
+++ framework/src/main/kotlin/org/utbot/typing/Symbol.kt
@@ -1,37 +1,32 @@
 package org.utbot.typing
-
 class TypeParameter(
     constraints: Set<TypeParameterConstraint>
 )
-
 open class TypeParameterConstraint
-
 open class Type(
     val parameters: List<TypeParameter>
 )
-
 sealed class Name
 data class Atom(val name: String): Name()
 data class FqName(val prefix: List<String>, val name: String): Name()
-
 open class ConcreteType(val name: Name) : Type(emptyList())
-
 open class PrimitiveType(name: Atom) : ConcreteType(name)
-
 class JavaInt : PrimitiveType(Atom("int"))
 class JavaBool : PrimitiveType(Atom("bool"))
 
+open class ParameterizedType (
+    parameters: List<TypeParameter>,
+    val type: Type
+) : Type(parameters)
 open class DeclaredType(
     fqName: FqName,
     val libraryType: Boolean
 ): ConcreteType(fqName)
-
 open class FunctionType(
     parameters: List<TypeParameter>,
     val arguments: List<Type>,
     val returnValue: Type
 ) : Type(parameters)
-
 open class Field(
     val name: String,
     val type: Type
